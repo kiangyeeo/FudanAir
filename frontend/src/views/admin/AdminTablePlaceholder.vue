b0a45f935a0158ee060afe5e0ac8063de1fe7ed9
kiangyeeo <wkengyi123@gmail.com>
2026-07-04 15:06:07 +0800

    admin view reconstruct

--- frontend/src/views/admin/AdminTablePlaceholder.vue
+++ frontend/src/views/admin/AdminTablePlaceholder.vue
@@ -13,14 +13,14 @@ defineProps<{
   <section class="page-section admin-table">
     <div class="toolbar">
       <h1 class="page-title">{{ title }}</h1>
-      <el-button type="primary">新增</el-button>
-      <el-button>刷新</el-button>
+      <el-button type="primary">Add</el-button>
+      <el-button>Refresh</el-button>
     </div>
     <el-table :data="[]" border>
       <el-table-column v-for="column in columns" :key="column.prop" :prop="column.prop" :label="column.label" :width="column.width" />
-      <el-table-column label="操作" width="150" fixed="right">
-        <el-button link type="primary">编辑</el-button>
-        <el-button link type="danger">删除</el-button>
+      <el-table-column label="Actions" width="150" fixed="right">
+        <el-button link type="primary">Edit</el-button>
+        <el-button link type="danger">Delete</el-button>
       </el-table-column>
     </el-table>
     <EmptyState :title="title" :description="description" />
